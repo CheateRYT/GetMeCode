c59dc8c9df6b221f7b9872f261ecd7f27148f4cf
Lev Rednikov <120374210+CheateRYT@users.noreply.github.com>
2024-12-14 15:55:40 +0300

    fix: typeit lags

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,27 +1,8 @@
-'use client'
-import { useEffect, useRef } from 'react'
 import { Link } from 'react-router-dom'
-import TypeIt from 'typeit'
+import TypeIt from 'typeit-react' // Импортируем TypeIt из typeit-react
 import styles from './index.module.css'
 
 const Main = () => {
-	const typeItRef = useRef<HTMLSpanElement | null>(null)
-
-	useEffect(() => {
-		if (typeItRef.current) {
-			const typeItInstance = new TypeIt(typeItRef.current, {
-				speed: 50,
-				waitUntilVisible: true,
-			})
-				.type('Добро пожаловать в GetMeCode — онлайн редактор кода')
-				.go()
-
-			return () => {
-				typeItInstance.destroy()
-			}
-		}
-	}, [])
-
 	return (
 		<div className={styles.container}>
 			<header className={styles.header}>
@@ -34,7 +15,14 @@ const Main = () => {
 			</header>
 			<main className={styles.main}>
 				<h2 className={styles.subtitle}>
-					<span ref={typeItRef}></span>
+					<TypeIt
+						options={{
+							speed: 50,
+							loop: true,
+						}}
+					>
+						Добро пожаловать в GetMeCode — онлайн редактор кода
+					</TypeIt>
 				</h2>
 				<p className={styles.description}>
 					Аналог leetcode.com где пользователь может писать код на разных
